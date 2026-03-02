df3a9be0878df63334bf32fd6dacceeac3e5ecce
muhammaddiyor <muhammaddiyor@gmail.com>
2026-03-02 13:39:05 +0500

    Video add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,14 @@ export default function Hero() {
         <HeroVideoDialog
           className="block dark:hidden"
           animationStyle="from-center"
-          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          videoSrc="https://www.youtube.com/embed/Bvj8Jvvsifc"
           thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
           thumbnailAlt="Hero Video"
         />
         <HeroVideoDialog
           className="hidden dark:block"
           animationStyle="from-center"
-          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          videoSrc="https://www.youtube.com/embed/Bvj8Jvvsifc"
           thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
           thumbnailAlt="Hero Video"
         />
