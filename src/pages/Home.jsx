828adb46c509e6d1da661d3c7fa1288656f712f6
muhammaddiyor <muhammaddiyor@gmail.com>
2026-03-02 12:20:23 +0500

    Loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,11 @@ import { Footer2 } from '../components/footer2';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 
-export default function Home() {
+export default function Home({ setLoading }) {
+  setLoading(true)
+  setTimeout(()=>{
+    setLoading(false)
+  }, 3000)
   return (
     <div className="container max-w-285 w-full mx-auto p-4">
       <Header />
